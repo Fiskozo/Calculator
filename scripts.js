d7bdb12bce2975a1148920a79b85eda6143650e4
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2024-04-05 22:51:08 +0200

    create basic math functions

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const add = function(x, y) {
+    let result = x + y;
+    return result;	
+  };
+  
+const subtract = function(x, y) {
+    let result = x - y;
+    return result;		
+  };
+
+const multiply = function(x, Y) {
+    let result = x * Y;
+    return result;
+};
+
+const divide = function(x, Y) {
+    let result = x / Y;
+    return result;
+};
+
