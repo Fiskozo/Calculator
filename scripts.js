1f2fda4f439d0d3626122d772e21c180c93b2956
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2024-05-06 17:41:36 +0200

    Fix so only one . can be pressed

--- scripts.js
+++ scripts.js
@@ -19,6 +19,7 @@ const display = document.querySelector('#display');
 display.classList.add('display');
 const clear = document.querySelector('#clear');
 clear.classList.add('clear');
+const c = document.querySelector('#c');
 const equals = document.querySelector('#equals');
 equals.classList.add('equals');
 const number = document.querySelectorAll('#number');
@@ -28,9 +29,40 @@ let number2;
 let displayValue;
 let selectedOperator;
 
+
+
+document.onkeyup = function (e) {
+  if (e.key >=0 && e.key <= 9) {
+    display.textContent += e.key;
+    displayValue = display.textContent; 
+  }else if(e.key === ',' || e.key === '.'){
+    if (display.textContent.match('.') != "."){
+    display.textContent += '.';
+    displayValue = display.textContent;
+    } 
+  }else if(e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' && selectedOperator == undefined && displayValue != undefined){
+    number1 = display.textContent;
+    display.textContent += e.key;
+    selectedOperator = e.key;
+  }else if(e.key === 'Backspace'){
+    let str = display.textContent
+    str = str.slice(0, -1);
+    display.textContent = str;
+  }else if(e.key === '=' || e.key === 'Enter'){
+    number2 = displayValue.split(selectedOperator)[1];
+    if (number1 == undefined || number2 == undefined || operator == undefined) {
+      console.error();
+    }else{
+      let result = operate(Number(number1), selectedOperator, Number(number2));
+      display.textContent = result.toFixed(2);
+      selectedOperator = undefined;
+    }}
+  };
 number.forEach(button => button.addEventListener('click', function() {
-    display.textContent += this.textContent;
-    displayValue = display.textContent;    
+  if (display.textContent.match('.') != "."){
+      display.textContent += this.textContent;
+      displayValue = display.textContent;
+      }    
 }))
 operator.forEach(button => button.addEventListener('click', function() {
     if (selectedOperator == undefined && displayValue != undefined) {
@@ -54,7 +86,12 @@ clear.addEventListener('click', function(){
   number1 = undefined;
   selectedOperator = undefined;
   number2 = undefined;
-  display.textContent = 0;
+  display.textContent = null;
+})
+c.addEventListener('click', function(){
+  let str = display.textContent
+  str = str.slice(0, -1);
+  display.textContent = str;
 })
 operate = function(number1, operator, number2) {
     if (operator == "÷" && number2 == "0"){
